9740ffd247dd2c8a58f360e3fc6869f675ed259d
HOLO-png <69238651+HOLO-png@users.noreply.github.com>
2022-12-01 23:00:30 +0700

    first commit

---
+++ src/main/java/com/example/cinema_back_end/entities/Role.java
@@ -0,0 +1,28 @@
+package com.example.cinema_back_end.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
